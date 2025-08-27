9ec68a9fdcc0a950a0e1d668b1a8ccde7c42ed61
minho <capurin@naver.com>
2025-08-27 15:03:25 +0900

    feature: 임시 포스터형 이미지로 대체

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,10 +2,11 @@
 
 import styled from "styled-components";
 
-const StyledImage = styled.iframe`
+const StyledIframeWrapper = styled.iframe`
     width: 100%;
     height: 100%;
 `;
+
 
 const MainWrapper = styled.div`
     display: flex;
@@ -16,13 +17,29 @@ const MainWrapper = styled.div`
     background-color: #f0f0f0;
 `;
 
+
+const Frame = styled.div`
+  width: 100%;
+  height: 100vh;   /* 부모가 명확한 높이를 가져야 함 */
+`;
+
+const ContainImg = styled.img`
+    width: 100%;
+    height: 100%;
+    object-fit: contain;  /* ✅ 전체 보임, 여백 생김 */
+    background: #f0f0f0;  /* 여백색 */
+`;
+
+
+
+
+
 export default function Home() {
   return (
       <MainWrapper>
-        <StyledImage
-            src="https://pf.kakao.com/_Aruexj/110537463"
-
-        />
+          <Frame>
+              <ContainImg src="/example.png" alt="example" />
+          </Frame>
       </MainWrapper>
   );
 }
